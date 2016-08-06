beee9f23ed4315ac913cf1daf8f44cb2c1cc4209
Nik Yim <yimkh92@gmail.com>
2016-08-06 17:23:49 +0800

    change API version from 1.2 to 2.0

--- databricks/automated-spark-cluster-creation/databricks-cluster-start.sh
+++ databricks/automated-spark-cluster-creation/databricks-cluster-start.sh
@@ -1,1 +1,10 @@
-echo $(date) "| start-cluster | " $(curl -X POST -H "Content-Type: application/json" -u username:password https://seekasia.cloud.databricks.com/api/1.2/clusters/create -d "@default-cluster-start.json") >> /path/to/your/log/cluster-status.log
+#!/bin/sh
+
+startCluster="$(curl -X POST -H "Content-Type: application/json" \
+-u username:password https://seekasia.cloud.databricks.com/api/2.0/clusters/create -d "@default-cluster-start.json")"
+
+# Put into log for logging purpose
+echo $(date) " | start-cluster | " $startCluster >> /Users/nicholedean/Documents/GitHub/python-batch/databricks/automated-spark-cluster-creation/cluster-status.log
+
+# Put into default-cluster-stop.json to use by databricks-cluster-stop.sh
+echo $startCluster > /Users/nicholedean/Documents/GitHub/python-batch/databricks/automated-spark-cluster-creation/default-cluster-stop.json
--- databricks/automated-spark-cluster-creation/databricks-cluster-stop.sh
+++ databricks/automated-spark-cluster-creation/databricks-cluster-stop.sh
@@ -1,1 +1,4 @@
-echo $(date) "| stop-cluster | " $(curl -X POST -H "Content-Type: application/json" -u username:password https://seekasia.cloud.databricks.com/api/1.2/clusters/delete -d "@default-cluster-stop.json") >> /path/to/your/log/cluster-status.log
+echo $(date) "| stop-cluster | " $(curl -X POST -H "Content-Type: application/json" -u username:password \
+https://seekasia.cloud.databricks.com/api/2.0/clusters/delete \
+-d "@default-cluster-stop.json") $(cat default-cluster-stop.json) >> \
+/Users/nicholedean/Documents/GitHub/python-batch/databricks/automated-spark-cluster-creation/cluster-status.log
