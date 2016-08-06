0dc432f735f9dce024e4b63c64bdb37b9e3e6648
Nik Yim <yimkh92@gmail.com>
2016-08-06 18:03:49 +0800

    mask databricks credential from the shell scripts

--- databricks/automated-spark-cluster-creation/databricks-cluster-start.sh
+++ databricks/automated-spark-cluster-creation/databricks-cluster-start.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+databricks_username=$(echo "$DATABRICKS_USERNAME")
+databricks_passwd=$(echo "$DATABRICKS_PASSWD")
+
 startCluster="$(curl -X POST -H "Content-Type: application/json" \
--u username:password https://seekasia.cloud.databricks.com/api/2.0/clusters/create -d "@default-cluster-start.json")"
+-u $databricks_username:$databricks_passwd https://seekasia.cloud.databricks.com/api/2.0/clusters/create -d "@default-cluster-start.json")"
 
 # Put into log for logging purpose
 echo $(date) " | start-cluster | " $startCluster >> /Users/nicholedean/Documents/GitHub/python-batch/databricks/automated-spark-cluster-creation/cluster-status.log
--- databricks/automated-spark-cluster-creation/databricks-cluster-stop.sh
+++ databricks/automated-spark-cluster-creation/databricks-cluster-stop.sh
@@ -1,4 +1,10 @@
-echo $(date) "| stop-cluster | " $(curl -X POST -H "Content-Type: application/json" -u username:password \
+#!/bin/sh
+
+databricks_username=$(echo "$DATABRICKS_USERNAME")
+databricks_passwd=$(echo "$DATABRICKS_PASSWD")
+
+echo $(date) "| stop-cluster | " $(curl -X POST -H "Content-Type: application/json" \
+-u $databricks_username:$databricks_passwd \
 https://seekasia.cloud.databricks.com/api/2.0/clusters/delete \
 -d "@default-cluster-stop.json") $(cat default-cluster-stop.json) >> \
 /Users/nicholedean/Documents/GitHub/python-batch/databricks/automated-spark-cluster-creation/cluster-status.log
